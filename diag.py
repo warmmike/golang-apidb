a845f573ca4b431601eeff97412adaaf331380d2
warmmike <mcarman@gmail.com>
2024-03-14 13:38:58 -0400

    Included SQS in diagram

--- diag.py
+++ diag.py
@@ -2,7 +2,7 @@ from diagrams import Diagram
 from diagrams.aws.compute import Lambda
 from diagrams.aws.database import Aurora
 from diagrams.aws.storage import S3
-from diagrams.aws.integration import Eventbridge
+from diagrams.aws.integration import Eventbridge,SQS
 from diagrams.aws.compute import EKS
 from diagrams.aws.network import APIGateway
 from diagrams.aws.general import Users
@@ -14,6 +14,7 @@ with Diagram("Golang Project API DB", show=True):
     aurora_db = Aurora("Amazon Aurora Postgres DB")
     s3_bucket = S3("Amazon S3 Bucket")
     event_bridge = Eventbridge("EventBridge")
+    sqs = SQS("SQS")
     lambda_function = Lambda("Lambda")
 
     usr = Users("API Users")
@@ -21,7 +22,8 @@ with Diagram("Golang Project API DB", show=True):
 
     provider >> s3_bucket
     s3_bucket >> event_bridge
-    event_bridge >> lambda_function
+    event_bridge >> sqs
+    sqs >> lambda_function
     lambda_function >> aurora_db
 
     usr >> gateway >> EKS_cluster >> aurora_db
